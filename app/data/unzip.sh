6db9f44163c69c20219865ca39036e2d19936130
Itay Katav <ikatav@redhat.com>
2026-02-10 15:45:33 +0200

    fix: unzip for linux
    Signed-off-by: Itay Katav <ikatav@redhat.com>

--- app/data/unzip.sh
+++ app/data/unzip.sh
@@ -1,4 +1,4 @@
 
 
-podman run --rm -v "$PWD:/work" -w /work alpine:3.19 sh -lc \
+podman run --rm -v "$PWD:/work:Z" -w /work alpine:3.19 sh -lc \
   "apk add --no-cache p7zip >/dev/null && 7z x combined-video-no-gap-rooftop.mp4.7z.001"
